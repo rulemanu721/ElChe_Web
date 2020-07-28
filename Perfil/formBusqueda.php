237c6955d8506489250bacc82b72886e6841f219
rulemanu721 <65323483+rulemanu721@users.noreply.github.com>
2020-07-28 15:26:13 -0500

    Add files via upload

---
+++ Perfil/formBusqueda.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div style="text-align: center;">
+        <img src="image/La.jpg" style="position: absolute; display: flex; width: 98%" >
+<form action="buscar.php" method="post" style="position: relative">
+    <br>
+    <div style="font-size: 32px;">Ingrese su numero de reservacion:</div>
+    <br>
+    <br>
+    <div><input type="text" name="codigo" placeholder="Numero de reservacion"></div>
+    <br>
+    <div><input type="submit" name="name" value="Buscar"></div>
+</form>
+</div>
+</body>
+</html>
